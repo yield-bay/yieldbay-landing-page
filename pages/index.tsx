3a1ebb9686bd16c787967a9009e4d7dc619e7433
nightwing-yb <nightwing-of-the-bay@protonmail.com>
2022-11-24 03:38:00 +0530

    meta tags added

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,25 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>YieldBay</title>
+        <meta name="title" content="YieldBay" />
+        <meta
+          name="description"
+          content="Yield Farming Hub for Polkadot & Kusama ecosystems"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.yieldbay.io/" />
+        <meta property="og:title" content="YieldBay" />
+        <meta
+          property="og:description"
+          content="Yield Farming Hub for Polkadot & Kusama ecosystems"
+        />
+
+        <meta property="twitter:url" content="https://www.yieldbay.io/" />
+        <meta property="twitter:title" content="YieldBay" />
+        <meta
+          property="twitter:description"
+          content="Yield Farming Hub for Polkadot & Kusama ecosystems"
+        />
       </Head>
       <div className="flex flex-col min-h-screen font bg-baseBlue text-white bg-bg-pattern-sm sm:bg-bg-pattern-md md:bg-bg-pattern bg-[center_top] sm:bg-center">
         <Header />
